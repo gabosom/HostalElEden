23c6b4fbfd542400981b2f755e3234513b083281
Jorge Gabuardi <jorge.gabuardi@live.com>
2013-12-25 18:07:07 -0500

    missing contact files

---
+++ Hotel/Models/ContactViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelEden.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+    }
+}
